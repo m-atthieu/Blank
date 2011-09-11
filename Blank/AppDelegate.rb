efd7d7d8d60a9893c7c64a4e9e3f9e748df1100b
Matthieu DESILE <matthieu@desile.fr>
2011-09-11 19:04:05 +0200

    Initial Commit

---
+++ Blank/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Blank
+#
+#  Created by Matthieu DESILE on 9/11/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ Blank/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blank
+//
+//  Created by Matthieu DESILE on 9/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
